e55ef83d986a8b16116688cdd7dce9dfafa7a92b
adlan jazuli <adlanjazuli@gmail.com>
2024-11-11 22:36:57 +0700

    initial commit

---
+++ services/config-server/src/test/java/com/maj/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maj.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
